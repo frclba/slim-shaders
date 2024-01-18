456b0c7d4e32a47538ae7e76301644b713cfaa70
Fernando Aguilar <fraguilar@pm.me>
2024-01-18 18:30:34 -0300

    Add cgmath library and implement ShaderProgram struct

--- engine/src/graphics/gl_wrapper.rs
+++ engine/src/graphics/gl_wrapper.rs
@@ -1,6 +1,11 @@
+use cgmath::Matrix;
 use gl::types::*;
-use std::mem;
+use std::collections::HashMap;
+use std::ffi::CString;
+use std::fs::File;
+use std::io::Read;
 use std::os::raw::*;
+use std::{mem, ptr};
 
 pub struct Vao {
     id: gl::types::GLuint,
@@ -125,6 +130,96 @@ impl VertexAttribute {
     pub fn disable(&self) {
         unsafe {
             gl::DisableVertexAttribArray(self.index);
+        }
+    }
+}
+
+pub struct ShaderProgram {
+    program_handle: u32,
+    uniform_ids: HashMap<String, u32>,
+}
+
+#[allow(temporary_cstring_as_ptr)]
+impl ShaderProgram {
+    pub fn new(vertex_shader_path: &str, fragment_shader_path: &str) -> ShaderProgram {
+        let mut vertex_shader_file = File::open(vertex_shader_path)
+            .unwrap_or_else(|_| panic!("Failed to open file {}", vertex_shader_path));
+        let mut fragment_shader_file = File::open(fragment_shader_path)
+            .unwrap_or_else(|_| panic!("Failed to open file {}", fragment_shader_path));
+
+        let mut vertex_shader_source = String::new();
+        let mut fragment_shader_source = String::new();
+
+        vertex_shader_file
+            .read_to_string(&mut vertex_shader_source)
+            .expect("Failed to read vertex shader file");
+
+        fragment_shader_file
+            .read_to_string(&mut fragment_shader_source)
+            .expect("Failed to read fragment shader file");
+
+        unsafe {
+            let vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
+            let c_str_vert = CString::new(vertex_shader_source.as_bytes()).unwrap();
+
+            gl::ShaderSource(vertex_shader, 1, &c_str_vert.as_ptr(), ptr::null());
+            gl::CompileShader(vertex_shader);
+
+            let fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
+            let c_str_frag = CString::new(fragment_shader_source.as_bytes()).unwrap();
+            gl::ShaderSource(fragment_shader, 1, &c_str_frag.as_ptr(), ptr::null());
+            gl::CompileShader(fragment_shader);
+
+            let program_handle = gl::CreateProgram();
+            gl::AttachShader(program_handle, vertex_shader);
+            gl::AttachShader(program_handle, fragment_shader);
+            gl::LinkProgram(program_handle);
+            gl::DeleteShader(vertex_shader);
+            gl::DeleteShader(fragment_shader);
+
+            ShaderProgram {
+                program_handle,
+                uniform_ids: HashMap::new(),
+            }
+        }
+    }
+
+    pub fn bind(&self) {
+        unsafe {
+            gl::UseProgram(self.program_handle);
+        }
+    }
+
+    pub fn unbind(&self) {
+        unsafe {
+            gl::UseProgram(0);
+        }
+    }
+
+    pub fn create_uniform(&mut self, uniform_name: &str) {
+        let uniform_location = unsafe {
+            gl::GetUniformLocation(
+                self.program_handle,
+                CString::new(uniform_name).unwrap().as_ptr(),
+            )
+        };
+
+        if uniform_location < 0 {
+            panic!("Could not find uniform {}", uniform_name);
+        } else {
+            self.uniform_ids
+                .insert(uniform_name.to_string(), uniform_location as u32);
+        }
+    }
+
+    pub fn set_matrix4fv_uniform(&self, uniform_name: &str, matrix: &cgmath::Matrix4<f32>) {
+        unsafe {
+            gl::UniformMatrix4fv(
+                self.uniform_ids[uniform_name] as i32,
+                1,
+                gl::FALSE,
+                matrix.as_ptr(),
+            );
         }
     }
 }
